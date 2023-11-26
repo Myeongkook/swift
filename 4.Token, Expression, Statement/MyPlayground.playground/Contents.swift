1a45252f5d032fc090b6b887ec735946cff52c32
myeongkook <myeongkuk@hotmail.com>
2023-11-26 19:22:12 +0900

    토큰, 표현식, 문장이란..

---
+++ 4.Token, Expression, Statement/MyPlayground.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+// Tokens == 언어를 표현하는 가장 작은 단위의 요소
+
+2 + 3; //
+
+
+/**
+ 토큰의 종류는 5가지로
+ 1.Identifiers
+ 2.Keywords
+ 3.Punctuations
+ 4.Operators
+ 5.Literals
+ */
+
+
+
+//공백을 여러개 입력해도 컴파일러는 하나의 공백으로 처리한다.
+
+
+
+// 값, 변수, 연산자, 함수 같은 것들이 하나 이상 모여서 하나의 값으로 표현되는
+// 코드를 Expressions(표현식)이라 한다
+
+
+
+/**
+ 정리
+ 1. Tokens = 토큰은 문법적으로 더이상 나눌 수 없는 단위이다.
+ 2. Expressions = 하나 이상의 토큰이 모여서 하나의 값을 도출하는 코드를 표현식이라 한다.
+ 3. Statements =  표현식이 하나 이상 모여서 특정 작업을 수행하는 코드를 문장이라 한다.
+ */
