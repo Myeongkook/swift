528a6813e9186ed97a56c678601331a5004450cd
myeongkook <myeongkuk@hotmail.com>
2023-11-26 18:26:34 +0900

    변수의 출력방법

---
+++ 2.HelloSwift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+import MapKit
+
+
+var greeting = "Hello, playground"
+
+
+print(greeting)
+
+var arr =  [String]()
+
+arr.append("mk")
+arr.append("hw")
+
+print(arr)
+dump(arr)
+
+// 어떠한 값을 출력하고자 할 땐 dump 또는 print 함수를 사용
+
